1a883e151ff8377ed1e011a536185c4cbda58a29
hvu160699 <hvu160699@gmail.com>
2019-05-25 18:11:59 +0700

    UI Main - lần 2

--- app/src/main/java/com/example/appchat/Fragment/FriendFragment.java
+++ app/src/main/java/com/example/appchat/Fragment/FriendFragment.java
@@ -4,12 +4,16 @@ package com.example.appchat.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.example.appchat.Adapter.ViewPagerFriend;
+import com.example.appchat.Fragment.Friend.FindFriendFragment;
+import com.example.appchat.Fragment.Friend.FriendListFragment;
 import com.example.appchat.R;
 
 /**
@@ -29,14 +33,49 @@ public class FriendFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_friend, container, false);
 
-        ViewPager viewPagerFriend = view.findViewById(R.id.viewPagerFriend);
-        TabLayout tabLayoutFriend = view.findViewById(R.id.tabLayoutFriend);
+        final TabLayout tabLayoutFriend = view.findViewById(R.id.tabLayoutFriend);
+        FrameLayout frameLayoutFriend = view.findViewById(R.id.frameLayoutFriend);
 
-        viewPagerFriend.setAdapter(new ViewPagerFriend(getChildFragmentManager(), this.getContext()));
-        tabLayoutFriend.setupWithViewPager(viewPagerFriend);
+        tabLayoutFriend.addTab(tabLayoutFriend.newTab().setText("Find friend"));
+        tabLayoutFriend.addTab(tabLayoutFriend.newTab().setText("List friend"));
+        tabLayoutFriend.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        Fragment defaultFragment = new FindFriendFragment();
+        setFragment(defaultFragment);
+
+
+        tabLayoutFriend.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tabLayoutFriend.getSelectedTabPosition() == 0){
+                    Fragment findFriend = new FindFriendFragment();
+                    setFragment(findFriend);
+                }
+                if (tabLayoutFriend.getSelectedTabPosition() == 1) {
+                    Fragment friendList = new FriendListFragment();
+                    setFragment(friendList);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
 
         return view;
+    }
+
+    public void setFragment(Fragment mFragment) {
+        FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayoutFriend, mFragment);
+        fragmentTransaction.commit();
     }
 
 }
